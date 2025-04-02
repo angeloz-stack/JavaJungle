85fc660c50b10ffec3318ce83f8a8143f1b77462
manuleo5 <emanueleleone254@gmail.com>
2025-04-02 15:10:46 +0200

    interfaccia e pianta

--- Pianta.java
+++ Pianta.java
@@ -1,1 +1,26 @@
 
+public class Pianta {
+    private boolean Poisonusus;
+    private float reproductive_rate; // (0,1]
+    private int energy; 
+    //costruttore 
+    public Pianta (boolean Poisonusus, float reproductive_rate, int energy){
+        this.Poisonusus=Poisonusus;
+        this.reproductive_rate=Math.max(Float.MIN_VALUE, Math.min(reproductive_rate, 1.0f)); 
+        // fa si che il tasso sia sempre tra (0,1] se è <0 lo setta a MIN_VALUE= 1.4E-45
+        this.energy=energy;
+
+
+
+    }
+
+
+
+}
+
+
+
+
+
+
+
